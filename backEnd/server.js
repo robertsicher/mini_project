b7a37ead51634fc6e49e70e00e9f0a27c6205fb6
Ard847 <renatopombal7@gmail.com>
2020-12-08 20:48:14 +0000

    adding the data base

--- backEnd/server.js
+++ backEnd/server.js
@@ -10,6 +10,8 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
 let reservations = [];
+let currentReservations =[];
+let allReservations = [];
 
 // Get objects for tables
 const getTables = (reservations) => {
@@ -37,6 +39,7 @@ server.get("/api/tables", function(req, res) {
 server.post("/reserve", function(req, res) {
   let newReservation = req.body;
   reservations.push(newReservation);
+  insert(reservations)
   res.json(newReservation);
 });
 
@@ -44,3 +47,57 @@ server.post("/reserve", function(req, res) {
 server.listen(port, () => {
     console.log("Server is listening in port ", port)
 });
+
+
+const mysql = require("mysql")
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    port: 3306,
+    user:'root',
+    password : "yourRootPassword",
+    database: 'miniproject'
+})
+
+connection.connect(function(err){
+  if(err){
+     // console.log("error ",err)
+  }
+  else{
+     // console.log("connected",connection)
+  }
+
+  
+  /* connection.query(`Insert into reservations(firstName,surname) values(${reservations.tables.name} , ${reservations.tables[0].PhoneNumber}, ${reservations.tables[0].EmailAdress})`,function(err,result){
+      if(err)console.log("error in query ", err)
+      else console.log("result", result)
+      hello(result)
+  }) */
+})
+
+
+ const insert = (reservations) => {
+  console.log(reservations)
+  currentReservations = reservations
+  console.log(currentReservations)
+  if(currentReservations.length > 1){
+    currentReservations.shift()
+  }
+  connection.query(`Insert into reservations (fullName,PhoneNumber,EmailAdress) VALUES ('${ currentReservations[0].name}' , '${ currentReservations[0].PhoneNumber}', '${ currentReservations[0].EmailAdress}');`,function(err,result){
+    if(err){
+      console.log("error in query ", err)
+    }else {
+      console.log("result", result)
+    }
+    api(result)
+  })
+
+}
+
+  const api = (result) => {
+  console.log(result)
+
+  connection.query(`Select * from reservations)`,function(err,result){
+    if(err)console.log("error in query ", err)
+    else console.log("result", result)})
+} 
